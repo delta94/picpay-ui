9a3532d966d3caa8f335a9012de18e9e2ce1e731
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-04-19 14:41:43 -0300

    Refactor navigation and render a custom component in the bottom tab

---
+++ src/components/PayButton/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function PayButton() {
+  return <Text style={{ color: '#fff' }}>Pay Button</Text>;
+}
--- src/navigation.js
+++ src/navigation.js
@@ -3,6 +3,8 @@ import { Dimensions } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AntDesign, Ionicons } from '@expo/vector-icons';
 
+import PayButton from './components/PayButton';
+
 import Home from './screens/Home';
 import Wallet from './screens/Wallet';
 import Pay from './screens/Pay';
@@ -19,10 +21,6 @@ const icons = {
     lib: AntDesign,
     name: 'creditcard',
   },
-  Pay: {
-    lib: AntDesign,
-    name: 'home',
-  },
   Notifications: {
     lib: Ionicons,
     name: 'ios-notifications-outline',
@@ -33,39 +31,49 @@ const icons = {
   },
 };
 
+const tabBarOption = {
+  style: {
+    backgroundColor: '#131418',
+    borderTopColor: 'rgba(255,255,255,.2',
+    height: height * 0.095,
+    paddingBottom: 5,
+  },
+  activeTintColor: '#fff',
+  inactiveTintColor: '#696969',
+};
+
+function configScreenOption({ route }) {
+  return {
+    tabBarIcon: ({ color, size }) => {
+      if (route.name === 'Pay') return <PayButton />;
+      const { lib: Icon, name } = icons[route.name];
+      return <Icon name={name} size={size} color={color} />;
+    },
+  };
+}
+
 export default function Navigation() {
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          const { lib: Icon, name } = icons[route.name];
-          return <Icon name={name} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        style: {
-          backgroundColor: '#131418',
-          borderTopColor: 'rgba(255,255,255,.2',
-          height: height * 0.095,
-          paddingBottom: 5,
-        },
-        activeTintColor: '#fff',
-        inactiveTintColor: '#696969',
-      }}
+      screenOptions={configScreenOption}
+      tabBarOptions={tabBarOption}
     >
       <Tab.Screen name="Home" component={Home} options={{ title: 'Início' }} />
+
       <Tab.Screen
         name="Wallet"
         component={Wallet}
         options={{ title: 'Carteira' }}
       />
-      <Tab.Screen name="Pay" component={Pay} options={{ title: 'Pagar' }} />
+
+      <Tab.Screen name="Pay" component={Pay} options={{ title: '' }} />
 
       <Tab.Screen
         name="Notifications"
         component={Pay}
         options={{ title: 'Notificações' }}
       />
+
       <Tab.Screen
         name="Settings"
         component={Pay}
